5dcdb3afa30340f3536d2c0372cf86f39ee3b449
firacil <firaolmekbib511@gmail.com>
2024-02-20 11:19:01 -0800

    updated

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+
+/**
+ * swapper - swap to nodes in a listint_t doubly linked list.
+ * @head: a pointer to head of DLL.
+ * @n1: fisrt node to be swapped
+ * @n2: second node to be swapped
+ *
+ * Return: nothing
+ */
+
+void swapper(listint_t **head, listint_t **n1, listint_t *n2)
+{
+}
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * 	in ascending ordr using insert sort algorithm
+ * @list: list to be sorted
+ *
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+}
