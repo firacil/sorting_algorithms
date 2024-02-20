b6f74f8d6751b7d83a85cb45e22326a04a422034
firacil <firaolmekbib511@gmail.com>
2024-02-20 12:10:44 -0800

    updated

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,10 +11,21 @@
 
 void swapper(listint_t **head, listint_t **n1, listint_t *n2)
 {
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*head = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 /**
  * insertion_sort_list - sorts a doubly linked list of integers
- * 	in ascending ordr using insert sort algorithm
+ * in ascending ordr using insert sort algorithm
  * @list: list to be sorted
  *
  * Return: Nothing
@@ -22,4 +33,19 @@ void swapper(listint_t **head, listint_t **n1, listint_t *n2)
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *i, *insert, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (i = (*list)->next; i != NULL; i = temp)
+	{
+		temp = i->next;
+		insert = i->prev;
+		while (insert != NULL && i->n < insert->n)
+		{
+			swapper(list, &insert, i);
+			print_list((const listint_t *)*list);
+		}
+	}
 }
